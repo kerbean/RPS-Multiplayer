66306aafc3104496545fd10ebb8c458c674d09f4
kerbean <kervindelrosario@yahoo.com>
2019-11-10 18:46:19 +0800

    TESTING GOOGLE SIGN IN

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -100,6 +100,7 @@ $(document).ready(function () {
                         uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
                         // this value to authenticate with your backend server, if
                         // you have one. Use User.getToken() instead.
+                        console.log("Google Name: " + name);
                         $(".welcome-note").append(name + "!");
                         showContainer();
 
