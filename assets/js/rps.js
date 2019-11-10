540bd4b4017455514caf9c3871dc3e1979e71850
kerbean <kervindelrosario@yahoo.com>
2019-11-10 17:19:09 +0800

    TESTING GOOGLE FORM SIGNIN

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -69,6 +69,7 @@ $(document).ready(function () {
                         uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
                         // this value to authenticate with your backend server, if
                         // you have one. Use User.getToken() instead.
+                        showContainer();
 
                     }
                 } else {
