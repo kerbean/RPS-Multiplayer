6d2a064494168925f52196300405cc892303d7cb
kerbean <kervindelrosario@yahoo.com>
2019-11-10 15:51:28 +0800

    TESTING GOOGLE SIGN-IN

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,4 +1,7 @@
-$(document).ready(() => {
+$(document).ready(function () {
+
+    console.log("javascript working");
+
     var firebaseConfig = {
         apiKey: "AIzaSyDZwxwL_PbIzAz1I4m3U1SfapOKxL2mI0Y",
         authDomain: "pomodoro-algo.firebaseapp.com",
@@ -13,58 +16,75 @@ $(document).ready(() => {
 
     database = firebase.database();
 
-    var provider = new firebase.auth.GoogleAuthProvider();
+    openLogin();
 
-    // firebase.auth().signInWithPopup(provider).then(function (result) {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     var token = result.credential.accessToken;
-    //     // The signed-in user info.
-    //     var user = result.user;
-    //     // ...
-    // }).catch(function (error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-    //     // ...
+    /*
+    
+        var provider = new firebase.auth.GoogleAuthProvider();
+    
+        // firebase.auth().signInWithPopup(provider).then(function (result) {
+        //     // This gives you a Google Access Token. You can use it to access the Google API.
+        //     var token = result.credential.accessToken;
+        //     // The signed-in user info.
+        //     var user = result.user;
+        //     // ...
+        // }).catch(function (error) {
+        //     // Handle Errors here.
+        //     var errorCode = error.code;
+        //     var errorMessage = error.message;
+        //     // The email of the user's account used.
+        //     var email = error.email;
+        //     // The firebase.auth.AuthCredential type that was used.
+        //     var credential = error.credential;
+        //     // ...
+        // });
+    
+        // function onSignIn(googleUser) {
+        //     var profile = googleUser.getBasicProfile();
+        //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        //     console.log('Name: ' + profile.getName());
+        //     console.log('Image URL: ' + profile.getImageUrl());
+        //     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        // }
+    
+        function onSignIn(googleUser) {
+            console.log('Google Auth Response', googleUser);
+            // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+            var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
+                unsubscribe();
+                // Check if we are already signed-in Firebase with the correct user.
+                if (!isUserEqual(googleUser, firebaseUser)) {
+                    // Build Firebase credential with the Google ID token.
+                    var credential = firebase.auth.GoogleAuthProvider.credential(
+                        googleUser.getAuthResponse().id_token);
+                    // Sign in with credential from the Google user.
+                    firebase.auth().signInWithCredential(credential).catch(function (error) {
+                        // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        // The email of the user's account used.
+                        var email = error.email;
+                        // The firebase.auth.AuthCredential type that was used.
+                        var credential = error.credential;
+                        // ...
+                    });
+                } else {
+                    console.log('User already signed-in Firebase.');
+                }
+            });
+        }
+    
+        */
+    // $(".open-button").on("click", function () {
+    //     $("#myForm").css("display", "block");
     // });
+    function openLogin() {
+        $("#myForm").css("display", "block");
+    }
 
-    // function onSignIn(googleUser) {
-    //     var profile = googleUser.getBasicProfile();
-    //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    //     console.log('Name: ' + profile.getName());
-    //     console.log('Image URL: ' + profile.getImageUrl());
-    //     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    // }
 
-    function onSignIn(googleUser) {
-        console.log('Google Auth Response', googleUser);
-        // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-        var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
-            unsubscribe();
-            // Check if we are already signed-in Firebase with the correct user.
-            if (!isUserEqual(googleUser, firebaseUser)) {
-                // Build Firebase credential with the Google ID token.
-                var credential = firebase.auth.GoogleAuthProvider.credential(
-                    googleUser.getAuthResponse().id_token);
-                // Sign in with credential from the Google user.
-                firebase.auth().signInWithCredential(credential).catch(function (error) {
-                    // Handle Errors here.
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    // The email of the user's account used.
-                    var email = error.email;
-                    // The firebase.auth.AuthCredential type that was used.
-                    var credential = error.credential;
-                    // ...
-                });
-            } else {
-                console.log('User already signed-in Firebase.');
-            }
-        });
+    function closeForm() {
+        $("#myForm").style.display = "none";
     }
 
 
