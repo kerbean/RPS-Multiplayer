f24daa678e27fae267958203b722fbb31a7fc368
kerbean <kervindelrosario@yahoo.com>
2019-11-10 17:02:50 +0800

    TESTING GOOGLE FORM SIGNIN

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
     openLogin();
 
 
-    var provider = new firebase.auth.GoogleAuthProvider();
+    // var provider = new firebase.auth.GoogleAuthProvider();
 
     // firebase.auth().signInWithPopup(provider).then(function (result) {
     //     // This gives you a Google Access Token. You can use it to access the Google API.
